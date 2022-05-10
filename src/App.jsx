e5968812a66c78505b3e2d6c3493d1c921a87130
Eduardo Cobuci <eduardo.cobuci@vareto.com>
2022-05-10 12:29:57 -0300

    Add ag-grid sample code

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,34 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useMemo, useState } from "react";
+import { AgGridReact } from "ag-grid-react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+const App = () => {
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celica", price: 35000 },
+    { make: "Ford", model: "Mondeo", price: 32000 },
+    { make: "Porsche", model: "Boxster", price: 72000 },
+  ]);
+
+  const [columnDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+  ]);
+
+  const defaultColDef = useMemo(() => ({ editable: true }), []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="ag-theme-alpine" style={{ height: 400, width: 800 }}>
+      <AgGridReact
+        defaultColDef={defaultColDef}
+        undoRedoCellEditing={true}
+        rowData={rowData}
+        columnDefs={columnDefs}
+      ></AgGridReact>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
